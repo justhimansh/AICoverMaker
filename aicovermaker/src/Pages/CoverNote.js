c79b9c9f7316e1c849d67048993dc3f7e39950d8
Himansh <justhimanshk@gmail.com>
2023-11-18 04:51:33 +1300

    v3.0

--- aicovermaker/src/Pages/CoverNote.js
+++ aicovermaker/src/Pages/CoverNote.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import './styles/buttonstyle.scss';
 
-const API_KEY = "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"; // Replace with your actual OpenAI API key
+const API_KEY = process.env.REACT_APP_OPENAI_KEY; // Replace with your actual OpenAI API key
 
 function CoverNote() {
   const [candidateSummary, setCandidateSummary] = useState('');
